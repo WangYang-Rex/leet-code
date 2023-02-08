f5cb7356e2348f856b36834ce600bae33b646dee
wangyang <wy617418875@gmail.com>
2023-02-08 15:01:22 +0800

    ✨ feat: reset

--- problemset/defanging-an-ip-address/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 正则
- * @desc 时间复杂度 O(N)  空间复杂度 O(1)
- * @param address
- * @returns
- */
-export function defangIPaddr(address: string): string {
-  return address.replace(/\./g, '[.]')
-}
--- problemset/encode-and-decode-tinyurl/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect, it } from 'vitest'
-import { decode, encode } from '.'
-
-it('TinyURL 的加密与解密', () => {
-  const url = 'https://leetcode.com/problems/design-tinyurl'
-  const tinyUrl = encode(url)
-  expect(tinyUrl).not.toBe(url)
-  expect(decode(tinyUrl)).toBe(url)
-})
