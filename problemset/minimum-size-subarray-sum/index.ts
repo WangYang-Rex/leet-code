4155025ffea817302beb9fb6b50c54fa455c3923
wangyang <wy617418875@gmail.com>
2023-02-16 18:07:48 +0800

    ✨ feat: 长度最小的子数组

--- problemset/minimum-size-subarray-sum/index.ts
+++ problemset/minimum-size-subarray-sum/index.ts
@@ -1,26 +1,28 @@
 /**
- * 暴力求解
+ * 滑动窗口
+ * @desc 时间复杂度 O(N)  空间复杂度 O(1)
  * @param target
  * @param nums
  * @returns
  */
 export function minSubArrayLen(target: number, nums: number[]): number {
-  let res = 0;
+  let left = 0;
+  let right = 0;
+  let sum = 0;
+  let minLen = Infinity;
 
-  for (let left = 0; left < nums.length; left++) {
-    let _sum = 0;
-    for (let right = left; right < nums.length; right++) {
-      _sum += nums[right];
-      if (_sum >= target) {
-        if (res === 0 || res > right - left + 1) {
-          res = right - left + 1;
-        }
-        break;
-      }
+  while (right < nums.length) {
+    sum += nums[right];
+    right++;
+
+    while (sum >= target) {
+      minLen = Math.min(minLen, right - left);
+      sum -= nums[left];
+      left++;
     }
   }
 
-  return res
+  return minLen === Infinity ? 0 : minLen;
 }
 
 /**
@@ -52,3 +54,28 @@ export function minSubArrayLen1(target: number, nums: number[]): number {
 
   return res
 }
+/**
+ * 暴力求解
+ * @param target
+ * @param nums
+ * @returns
+ */
+export function minSubArrayLen2(target: number, nums: number[]): number {
+  let res = 0;
+
+  for (let left = 0; left < nums.length; left++) {
+    let _sum = 0;
+    for (let right = left; right < nums.length; right++) {
+      _sum += nums[right];
+      if (_sum >= target) {
+        if (res === 0 || res > right - left + 1) {
+          res = right - left + 1;
+        }
+        break;
+      }
+    }
+  }
+
+  return res
+}
+
